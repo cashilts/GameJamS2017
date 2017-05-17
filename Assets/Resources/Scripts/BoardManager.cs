424a51bebb94f4ec674dc5376ca1e14081b0b899
Cashilts <christopherhilts@gmail.com>
2017-05-17 04:12:35 -0400

    Created more tile models and created basic board layout proceedure

---
+++ Assets/Resources/Scripts/BoardManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+    const float tileWidth = 1.7f;
+    const float tileHeight = 1.5f;
+    public int boardSize = 100;
+
+	// Use this for initialization
+	void Start () {
+        float startX = (boardSize / 2) * tileWidth * -1;
+        float startY = (boardSize / 2) * tileHeight * -1;
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++){
+                GameObject newTile = GameObject.Instantiate(newTile = (GameObject)Resources.Load("Prefabs/DeepWaterTex"));
+                newTile.name = "tile" + j + i;
+                newTile.transform.position = new Vector3(startX + (tileWidth * j), 0, startY + (tileHeight * i));
+            }
+            if (i % 2 == 0){
+                startX -= tileWidth / 2;
+            }
+            else {
+                startX += tileWidth / 2;
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
