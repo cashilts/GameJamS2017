a3f4133ffdee5197b2aacd4c7a054d25e093b9ad
christopherhilts@gmail.com <christopherhilts@gmail.com>
2017-06-02 15:48:54 -0700

    Added pointer for vive controller

--- Assets/Resources/Scripts/TileSelector.cs
+++ Assets/Resources/Scripts/TileSelector.cs
@@ -3,20 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TileSelector : MonoBehaviour {
-
+    LineRenderer trace;
 	// Use this for initialization
 	void Start () {
-		
+        trace = GetComponent<LineRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        int tileX = Random.Range(0, 99);
-        int tileY = Random.Range(0, 99);
-        MeshRenderer currentMesh = GameObject.Find("tile" + tileX + "," + tileY).transform.GetChild(0).GetComponent<MeshRenderer>();
-        Material[] newMaterials = new Material[2];
-        newMaterials[0] = currentMesh.material;
-        newMaterials[1] = (Material)Resources.Load("Models/Materials/Highlight");
-        currentMesh.materials = newMaterials;
-	}
+        Vector3 fwd = transform.TransformDirection(transform.forward);
+        Debug.Log(fwd);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, fwd,out hit, 30)) {
+            Debug.DrawLine(transform.position, hit.point);
+            trace.SetPosition(0, transform.position);
+            trace.SetPosition(1, hit.point);
+            MeshRenderer currentMesh = hit.collider.GetComponent<MeshRenderer>();
+            Material[] newMaterials = new Material[2];
+            newMaterials[0] = currentMesh.material;
+            newMaterials[1] = (Material)Resources.Load("Models/Materials/Highlight");
+            currentMesh.materials = newMaterials;
+        }
+        //int tileX = Random.Range(0, 99);
+        //int tileY = Random.Range(0, 99);
+
+    }
 }
