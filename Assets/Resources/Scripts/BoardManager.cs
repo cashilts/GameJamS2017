9b62d93b6dd7a5c0973b7603bd47074207bd0d63
Cashilts <christopherhilts@gmail.com>
2017-07-05 02:54:27 -0400

    Added procedureal generation for ice and continents

--- Assets/Resources/Scripts/BoardManager.cs
+++ Assets/Resources/Scripts/BoardManager.cs
@@ -7,6 +7,7 @@ public class BoardManager : MonoBehaviour {
     public const float tileWidth = 1.7f;
     public const float tileHeight = 1.5f;
     public const int boardSize = 100;
+    Tile[,] board = new Tile[boardSize,boardSize];  
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +19,7 @@ public class BoardManager : MonoBehaviour {
                 newTile.name = "tile" + j + "," + i;
                 newTile.transform.position = new Vector3(startX + (tileWidth * j), 0, startY + (tileHeight * i));
                 newTile.transform.parent = this.transform;
+                board[i, j] = newTile.GetComponent<Tile>();
             }
             if (i % 2 == 0){
                 startX -= tileWidth / 2;
@@ -26,10 +28,83 @@ public class BoardManager : MonoBehaviour {
                 startX += tileWidth / 2;
             }
         }
+        int iceX = (int)Random.Range(0.4f * boardSize, 0.6f * boardSize);
+        GenerateIce(iceX, 0, 100);
+        iceX = (int)Random.Range(0.4f * boardSize, 0.6f * boardSize);
+        GenerateIce(iceX, boardSize - 1, 100);
+
+        int continentP = 100;
+        while (Random.Range(0, 100) <= continentP) {
+            int x = (int)Random.Range(boardSize * 0.3f, boardSize * 0.7f);
+            int y = (int)Random.Range(0, boardSize-1);
+            if (board[x, y].setType == Tile.tileType.DeepWater) {
+                GenerateContinent(x, y, continentP);
+                continentP -= 21;
+            }
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
+
+    void GenerateContinent(int x, int y, float p){
+        board[x, y].setType = Tile.tileType.Grass;
+        board[x, y].GetComponent<MeshRenderer>().material = (Material)Resources.Load("Models/Materials/GrassTex");
+        if (Random.Range(0, 100) <= p)
+        {
+            if (x - 1 >= 0)
+            {
+                if (board[x - 1, y].setType == Tile.tileType.DeepWater ) GenerateContinent(x - 1, y, p - 0.21f);
+            }
+        }
+        if (Random.Range(0, 100) <= p)
+        {
+            if (x + 1 <= boardSize - 1)
+            {
+                if (board[x + 1, y].setType == Tile.tileType.DeepWater) GenerateContinent(x + 1, y, p - 0.22f);
+            }
+        }
+        if (Random.Range(0, 100) <= p)
+        {
+            if (y + 1 <= boardSize - 1)
+            {
+                if (board[x, y + 1].setType == Tile.tileType.DeepWater) GenerateContinent(x, y + 1, p - 0.25f);
+            }
+        }
+        if (Random.Range(0, 100) <= p)
+        {
+            if (y - 1 >= 0)
+            {
+                if (board[x, y - 1].setType == Tile.tileType.DeepWater) GenerateContinent(x, y - 1, p - 0.20f);
+            }
+        }
+    }
+
+    void GenerateIce(int x, int y, float p) {
+        board[x, y].setType = Tile.tileType.Ice;
+        board[x, y].GetComponent<MeshRenderer>().material = (Material)Resources.Load("Models/Materials/IceTex");
+        if (Random.Range(0, 100) <= p)
+        {
+            if (x - 1 >= 0) {
+                if (board[x - 1, y].setType != Tile.tileType.Ice) GenerateIce(x - 1, y, p - 0.19f);
+            }
+        }
+        if (Random.Range(0, 100) <= p)
+        {
+            if (x + 1 <= boardSize-1) {
+                if (board[x + 1, y].setType != Tile.tileType.Ice) GenerateIce(x + 1, y, p - 0.19f);
+            }
+        }
+        if (Random.Range(0, 100) <= p) {
+            if (y + 1 <= boardSize - 1){
+                if (board[x, y + 1].setType != Tile.tileType.Ice) GenerateIce(x, y + 1, p - 10);
+            }
+        }
+        if (Random.Range(0, 100) <= p) {
+            if (y - 1 >= 0) {
+                if (board[x, y - 1].setType != Tile.tileType.Ice) GenerateIce(x, y - 1, p-5);
+            }
+        }
+    }
 }
---
+++ Assets/Resources/Scripts/Tile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+    public enum tileType {DeepWater, ShallowWater,Ice, Grass};
+    public tileType setType = tileType.DeepWater;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
