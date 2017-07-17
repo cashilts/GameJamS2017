3e1326ddec4639ca0d83936bef4d9f3aab65c73c
christopherhilts@gmail.com <christopherhilts@gmail.com>
2017-07-17 15:16:53 -0700

    Fixed tile selector to only select one tile at a time and changed orientation of player relative to board

--- Assets/Resources/Scripts/TileSelector.cs
+++ Assets/Resources/Scripts/TileSelector.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class TileSelector : MonoBehaviour {
     LineRenderer trace;
+    MeshRenderer previousMesh;
 	// Use this for initialization
 	void Start () {
         trace = GetComponent<LineRenderer>();
+        previousMesh = GameObject.Find("tile0,0").GetComponent<MeshRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -14,14 +16,21 @@ public class TileSelector : MonoBehaviour {
         Vector3 fwd = transform.TransformDirection(transform.forward);
         RaycastHit hit;
         if (Physics.Raycast(transform.position, fwd,out hit, 30)) {
-            Debug.DrawLine(transform.position, hit.point);
             trace.SetPosition(0, transform.position);
             trace.SetPosition(1, hit.point);
             MeshRenderer currentMesh = hit.collider.GetComponent<MeshRenderer>();
-            Material[] newMaterials = new Material[2];
-            newMaterials[0] = currentMesh.material;
-            newMaterials[1] = (Material)Resources.Load("Models/Materials/Highlight");
-            currentMesh.materials = newMaterials;
+            if (currentMesh != previousMesh)
+            {
+
+                Material[] newMaterials = new Material[2];
+                newMaterials[0] = currentMesh.material;
+                newMaterials[1] = (Material)Resources.Load("Models/Materials/Highlight");
+                currentMesh.materials = newMaterials;
+                newMaterials = new Material[1];
+                newMaterials[0] = previousMesh.materials[0];
+                previousMesh.materials = newMaterials;
+                previousMesh = currentMesh;
+            }
         }
         //int tileX = Random.Range(0, 99);
         //int tileY = Random.Range(0, 99);
